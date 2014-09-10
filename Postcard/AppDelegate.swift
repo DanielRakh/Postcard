50cd20b91fd3eee19de1a6c8186cd88ebe86a7ba
Daniel Rakhamimov <danielrak@me.com>
2014-09-10 14:47:04 -0400

    Add comment to test commits

--- Postcard/AppDelegate.swift
+++ Postcard/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
+        
+        //Test commits
+        
+        
         return true
     }
 
